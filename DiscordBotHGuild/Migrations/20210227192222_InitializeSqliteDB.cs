340178c8bcad9a573c3399c870ec75f0e39e6e9d
Jake-R17 <74597396+Jake-R17@users.noreply.github.com>
2021-02-27 21:20:05 +0100

    Stuff

---
+++ DiscordBotHGuild/Migrations/20210227192222_InitializeSqliteDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DiscordBotHGuild.Migrations
+{
+    public partial class InitializeSqliteDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MutedUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MemberId = table.Column<string>(type: "TEXT", nullable: true),
+                    MutedReason = table.Column<string>(type: "TEXT", nullable: true),
+                    MutedExpiration = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MutedUsers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MutedUsers");
+        }
+    }
+}
